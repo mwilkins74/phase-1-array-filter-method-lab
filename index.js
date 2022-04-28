f1694d9e1ba397eb9e8b4ece84a764fbb26674ae
Mark <markwilkins86@gmail.com>
2022-04-28 13:33:51 -0400

    commit tree 253f106b82f20c295b71a0044d753f067f9d26e8

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+// function findMatching(drivers, names) {
+    // return drivers.filter((matchNames) => matchNames() === names());
+// }
+
+function findMatching(drivers, string) {
+    return drivers.filter(
+      (name) => name.toLowerCase() === string.toLowerCase()
+    );
+  }
+
+function fuzzyMatch(drivers, string) {
+    // if(array.length = 0) {
+        return drivers.filter(
+            (people) => people.charAt(0) === string.charAt(0)
+        )
+    }
+function matchName(array, string) {
+    return array.filter((obj) => obj.name === string
+    )
+}
